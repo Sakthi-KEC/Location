a6863e244f07ed323d41f03c84f7e56da56df89e
Sakthi-KEC <97332413+Sakthi-KEC@users.noreply.github.com>
2022-03-22 18:31:50 +0530

    Update LocationController.java

--- src/main/java/net/microservices/Location/controller/LocationController.java
+++ src/main/java/net/microservices/Location/controller/LocationController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/location")
 
 public class LocationController
